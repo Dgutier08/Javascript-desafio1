c730932728f85f632ddb0f87a474662b719c5a26
Dgutier08 <102337196+Dgutier08@users.noreply.github.com>
2022-07-07 15:47:01 -0400

    .

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let boton = document.querySelector("#button");
+console.log("boton", boton);
+boton.addEventListener("click", () => {
+    let numero=document.querySelector("#input-1");
+    let total = document.querySelector("#total");
+    let cantidad =document.querySelector("#cantidad");
+    total.innerHTML = 400000 * numero.value;
+    cantidad.innerHTML = numero.value;
+    let colorlaptopt = document.querySelector(".circulo");
+    let color=document.querySelector("#input-2");
+    colorlaptopt.style.color = color.value;
+})
